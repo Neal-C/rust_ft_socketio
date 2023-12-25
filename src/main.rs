cf3be7597ab9e3156040193fd49ed62c887aeb51
Neal-C <pop6d531@duck.com>
2023-12-25 11:44:44 +0100

    feat(MAIN): axum setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+
+    let app: axum::Router<_> = axum::Router::<_>::new().route("/", get(|| async { "heartbeat" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    info!("Server started");
+    Ok(())
 }
