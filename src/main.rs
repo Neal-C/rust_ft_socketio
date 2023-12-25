180bf2d9e40db5ee7205e3106b78e476c4ec6c13
Neal-C <pop6d531@duck.com>
2023-12-25 14:17:12 +0100

    feat(MAIN): added Socketio setup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use axum::routing::get;
+use socketioxide::{extract::SocketRef, SocketIo};
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
+
+async fn on_connect(socket: SocketRef) {
+    info!("socket connected {}", socket.id)
+}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing::subscriber::set_global_default(FmtSubscriber::default())?;
 
-    let app: axum::Router<_> = axum::Router::<_>::new().route("/", get(|| async { "heartbeat\n" }));
+    let (socketio_layer, io) = SocketIo::new_layer();
+
+    io.ns("/", on_connect);
+
+    let app: axum::Router<_> = axum::Router::<_>::new()
+        .route("/", get(|| async { "heartbeat\n" }))
+        .layer(socketio_layer);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
